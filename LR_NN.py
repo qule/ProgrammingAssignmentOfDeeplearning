80a7665f1d1205172c72df984cc0a735e10a5e7e
allenwxf <allen_wangxufeng@163.com>
2018-05-31 11:29:07 +0800

    should start 1.3

--- LR_NN.py
+++ LR_NN.py
@@ -12,7 +12,7 @@ train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_datas
 
 #Example of a picture
 index = 26
-plt.imshow(train_set_x_orig[index])
+# plt.imshow(train_set_x_orig[index])
 print ("y = " + str(train_set_y[:, index]) + ", it's a '" +
        classes[np.squeeze(train_set_y[:,index])].decode("utf-8") + "' picture.")
 
@@ -254,5 +254,24 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
 
 
 
-d = model(X_train=train_set_x, Y_train=train_set_y, X_test=test_set_x, Y_test=test_set_y, num_iterations=2000,
-          learning_rate=0.005, print_cost=True)
+# d = model(X_train=train_set_x, Y_train=train_set_y, X_test=test_set_x, Y_test=test_set_y, num_iterations=2000,
+#           learning_rate=0.005, print_cost=True)
+
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+for i in learning_rates:
+    print ("the learning_rate is: " + str(i))
+    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations= 1500, learning_rate= i,
+                           print_cost= False)
+    print('\n' + "-----------------------------------------" + '\n')
+
+for i in learning_rates:
+    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+
+plt.ylabel("cost")
+plt.xlabel("iterations")
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+plt.show()
